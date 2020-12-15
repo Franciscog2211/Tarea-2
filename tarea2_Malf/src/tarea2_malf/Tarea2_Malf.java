cc11d664cc6755375f456d310371cc66cabf9873
PabloValenzuelaa <valenzuelapablo1998@gmail.com>
2020-12-14 23:55:58 -0300

    Entrada/Salida

--- tarea2_Malf/src/tarea2_malf/Tarea2_Malf.java
+++ tarea2_Malf/src/tarea2_malf/Tarea2_Malf.java
@@ -7,6 +7,7 @@ package tarea2_malf;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -39,6 +40,9 @@ public class Tarea2_Malf {
                 System.out.println("programa sigue");
             }
             expresiones.add(expresion);
+            if (expresion.contains("read") || expresion.contains("write")) {
+            	entradaSalida(expresion);
+            }
             Operacion operacion = new Operacion(expresion);
             for(int i=0; i<variables.size(); i++){
                 System.out.println("valor de: "+variables.get(i)+" es "+valores.get(i));
@@ -48,4 +52,53 @@ public class Tarea2_Malf {
       
     }
     
+    public static void entradaSalida(String comando) {
+    	String [] partes;
+    	partes = comando.split(" ");
+    	for(int i=0 ; i<partes.length ; i++) {
+    		if(partes[i].equals("read")) {
+    			String [] caracteres; 
+    	    	caracteres = partes[i+1].split("");
+    	    	BigInteger var = leer();
+    	    	variables.add(partes[i+1].substring(1, partes[i+1].length()));
+    	    	valores.add(var);
+    		}
+    		if(partes[i].equals("write")) {
+    			for(int j=0; j<variables.size() ;j++) {
+    				System.out.println("-----");
+    				if(partes[i+1].substring(1, partes[i+1].length()).equals(variables.get(j))) {
+    					System.out.println(variables.get(j)+" = "+valores.get(j));
+    				}
+    			}
+    			
+    		}
+    	}
+    }
+
+	private static BigInteger leer() {
+		Scanner read = new Scanner(System.in);
+		String num = "a";
+		while(!esNumero(num)) {
+			num = read.nextLine();
+		}
+		BigInteger numero = new BigInteger(num); 
+		return numero;
+	}
+	
+	private static boolean esNumero(String cadena){
+		if(cadena.length()==0) {
+			System.out.println("Solo puede ingresar numeros");
+			return false;
+		}
+		for (int i=0; i <cadena.length (); i++) { 
+			char c = cadena.charAt(i);
+			if(c<'0' || c>'9') {
+				System.out.println("Solo puede ingresar numeros");
+				return false;
+			}
+		}
+		System.out.println("Perfecto"+cadena);
+		return true;
+	}
+	
 }
